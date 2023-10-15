3bbf4dc4d0ee84eda59b69d7894157a83f08a2bc
Milena Siqueira <milematoss1@gmail.com>
2023-10-15 18:33:04 -0300

    feat: route to get goals

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 const indicatorController = require('./indicatorController')
 const keywordController = require('./keywordController')
+const goalController = require('./goalController')
 
-module.exports = { indicatorController, keywordController }
+module.exports = { indicatorController, keywordController, goalController }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { keywordController, indicatorController } = require('./controllers')
+const { keywordController, indicatorController, goalController } 
+  = require('./controllers')
 
 const router = express.Router();
 
 router.get("/keywords", keywordController.get);
 
 router.get("/indicators", indicatorController.get);
+
+router.post("/goals", goalController.get);
 
 module.exports = router
