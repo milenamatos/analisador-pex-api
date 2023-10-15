fd33f9fa1621e3de914d3d2104cc68a0df0f11ea
Milena Siqueira <milematoss1@gmail.com>
2023-10-15 18:32:52 -0300

    feat: goal controller

---
+++ src/controllers/goalController.js
@@ -0,0 +1,54 @@
+const { Op, Sequelize } = require("sequelize");
+const { Crossings } = require('../../db/models')
+
+exports.get = async (req, res) => {
+	const { goals, indicators } = req.body
+    
+	const directRelations = await Crossings.findAll({
+		attributes: [
+			'indicator_id',
+			[Sequelize.fn('array_agg', Sequelize.col('goal_id')), 'goals']
+		],
+		group: 'indicator_id',
+		where: {
+			[Op.and] : [
+				{ indicator_id: indicators },
+				{ [Op.or]: {
+					relation: 'X',
+					[Op.and]: [
+						{ relation: 'O'},
+						{ goal_id: goals }
+					]}
+				}
+			]
+		}
+    });
+
+	const indirectRelations = await Crossings.findAll({
+		attributes: [
+			'indicator_id',
+			[Sequelize.fn('array_agg', Sequelize.col('goal_id')), 'goals']
+		],
+		group: 'indicator_id',
+		where: {
+			[Op.and] : [
+				{ indicator_id: indicators },
+				{ [Op.or]: {
+					relation: 'I',
+					[Op.and]: [
+						{ relation: 'IO'},
+						{ goal_id: goals }
+					]}
+				}
+			]
+		}
+    });
+
+		const data = indicators.map(item => ({
+			indicator_id: item,
+			direct: directRelations.find(d => d.indicator_id === item)?.dataValues?.goals || [],
+			indirect: indirectRelations.find(d => d.indicator_id === item)?.dataValues?.goals || []
+		}))
+
+    return res.status(200).json(data);
+}
