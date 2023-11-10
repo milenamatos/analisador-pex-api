31554f962860a0f0e333a77b782f0c20ce0eb8df
Milena Siqueira <milematoss1@gmail.com>
2023-11-09 21:43:11 -0300

    feat: return points in analysis

--- src/controllers/analysisController.js
+++ src/controllers/analysisController.js
@@ -1,9 +1,12 @@
 const goalController = require('./goalController')
+const crossingsController = require('./crossingsController')
 
 exports.analyse = async (req, res) => {
 		const relatedGoals = await goalController.getRelated(req.body)
 
     const goalsDistribution = await goalController.getDistribution(req.body)
 
-    return res.status(200).json({ relatedGoals, goalsDistribution });
+    const pointsDistribution = await crossingsController.getPoints(req.body)
+
+    return res.status(200).json({ relatedGoals, goalsDistribution, pointsDistribution });
 }
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,6 @@ const indicatorController = require('./indicatorController')
 const keywordController = require('./keywordController')
 const goalController = require('./goalController')
 const analysisController = require('./analysisController')
+const crossingsController = require('./crossingsController')
 
-module.exports = { indicatorController, keywordController, goalController, analysisController }
+module.exports = { indicatorController, keywordController, goalController, analysisController, crossingsController }
