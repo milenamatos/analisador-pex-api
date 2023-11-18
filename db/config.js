f7b7fe6607d81898580c4dea477c67a0464cafc1
Milena Siqueira <milematoss1@gmail.com>
2023-11-18 19:50:41 -0300

    config: reject ssl

--- db/config.js
+++ db/config.js
@@ -2,24 +2,30 @@ const fs = require('fs');
 
 module.exports = {
   development: {
-    "username": "postgres",
-    "password": "admin",
-    "database": process.env.DB_DATABASE || "apex",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: "postgres",
+    password: "admin",
+    database: process.env.DB_DATABASE || "apex",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
   test: {
-    "username": "root",
-    "password": null,
-    "database": process.env.DB_DATABASE || "apex",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: "root",
+    password: null,
+    database: process.env.DB_DATABASE || "apex",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
   production: {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    }
   }
 }
