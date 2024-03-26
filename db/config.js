7ad0a3a183d629e9bfcf029d838a0f3dda12a6e7
Milena Siqueira <milematoss1@gmail.com>
2024-03-26 20:21:16 -0300

    fix: env

--- db/config.js
+++ db/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
